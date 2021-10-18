66f9b02f17ec7f663f1959d9c66a82a8acb2340b
Tshimangadzo Mudzanani <Tshimangadzo@users.noreply.github.com>
2021-10-18 21:19:52 +0100

    Update task0.8.js

--- task0.8.js
+++ task0.8.js
@@ -1,9 +1,10 @@
 function convert(number) {
   let hours = ~~(number / 60); //For large number like 10000000000 = 1410065408 this(~~) will not work as much as Math.floor,parseInt etc does not work
   let minutes = number % 60;
-  if (hours !== 0) hours += hours === 1 ? " hour" : " hours";
 
-  if (minutes !== 0) minutes += minutes === 1 ? " minute" : " minutes";
+  hours += hours === 1 ? " hour, " : " hours, ";
 
-  return hours === 0 ? minutes : minutes === 0 ? hours : hours + ", " + minutes;
+  minutes += minutes === 1 ? " minute" : " minutes";
+
+  return hours + minutes;
 }
