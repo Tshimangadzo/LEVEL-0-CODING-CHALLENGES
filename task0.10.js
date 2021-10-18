52f080bcad93555d5aaa56f756ca1ad680a0d9ee
Tshimangadzo Mudzanani <Tshimangadzo@users.noreply.github.com>
2021-10-18 21:49:27 +0100

    Update task0.10.js

--- task0.10.js
+++ task0.10.js
@@ -1,17 +1,32 @@
 function getCommonLetters(word1, word2) {
-  var commonLetters = "Common letters: ";
-  var trackCommas = 0;
+  let saveCommonLetters = [];
+
   for (let i = 0; i < word2.length; i++) {
     for (let j = 0; j < word1.length; j++) {
       if (word1[j].toLowerCase() === word2[i].toLowerCase()) {
-        if (trackCommas === 0) {
-          commonLetters += word1[j].toLowerCase();
-          trackCommas++;
-        } else {
-          commonLetters = commonLetters + ", " + word1[j].toLowerCase();
-        }
+        saveCommonLetters.push(word1[j].toLowerCase());
       }
     }
   }
+
+  for (let i = 0; i < saveCommonLetters.length; i++) {
+    let countLetters = 0;
+    for (let j = 0; j < saveCommonLetters.length; j++) {
+      if (saveCommonLetters[i] == saveCommonLetters[j]) {
+        countLetters++;
+      }
+      if (countLetters > 1) {
+        saveCommonLetters.splice(j, 1);
+        j = j + 1;
+      }
+    }
+  }
+
+  let commonLetters = "Common letters: ";
+  for (let i = 0; i < saveCommonLetters.length; i++) {
+    commonLetters += saveCommonLetters[i];
+    if (i < saveCommonLetters.length - 1) commonLetters += ", ";
+  }
+
   console.log(commonLetters);
 }
