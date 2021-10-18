fa4c0eb4dc86c9fbcd437e08ee7fc1857623c81c
Tshimangadzo Mudzanani <Tshimangadzo@users.noreply.github.com>
2021-10-18 15:44:48 +0100

    Update task0.9.js

--- task0.9.js
+++ task0.9.js
@@ -1,18 +1,35 @@
 function getVowels(word) {
-  var vowels = ["a", "e", "i", "o", "u"];
-  var extractVowels = "Vowels: ";
-  var trackComma = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+
+  word = word.toLowerCase();
+
+  let holdVowels = [];
   for (let i = 0; i < word.length; i++) {
     for (let j = 0; j < vowels.length; j++) {
       if (word[i] === vowels[j]) {
-        if (trackComma === 0) {
-          extractVowels += word[i];
-          trackComma++;
-        } else {
-          extractVowels = extractVowels + ", " + word[i];
-        }
+        holdVowels.push(vowels[j]);
       }
     }
   }
-  console.log(extractVowels);
+
+  for (let i = 0; i < holdVowels.length; i++) {
+    let countVowels = 0;
+    for (let j = 0; j < holdVowels.length; j++) {
+      if (holdVowels[i] == holdVowels[j]) {
+        countVowels++;
+      }
+      if (countVowels > 1) {
+        holdVowels.splice(j, 1);
+        j = j + 1;
+      }
+    }
+  }
+
+  let vowelsToString = "Vowels: ";
+  for (let i = 0; i < holdVowels.length; i++) {
+    vowelsToString += holdVowels[i];
+    if (i < holdVowels.length - 1) vowelsToString += ", ";
+  }
+
+  console.log(vowelsToString);
 }
